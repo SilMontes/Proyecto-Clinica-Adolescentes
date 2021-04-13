71e3d5edfe700612722b3a167f6edbf49512eebd
Kimur1 <kimur53@gmail.com>
2021-04-13 22:37:55 +0000

    footer.html

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
 
+import { Row, Col } from "react-bootstrap";
+
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+		<Row>
+			<Col>
+				<h6>Sobre Nosotros</h6>
+				<p>Contáctenos</p>
+				<p>Reportá</p>
+				<p>Comunidad juventud</p>
+				<p>Clínicas cercanas</p>
+			</Col>
+			<Col>
+				<h4>Clinica de Adolescentes</h4>
+				<i className="fab fa-facebook-f" />
+				<i className="fab fa-instagram" />
+				<i className="fab fa-youtube" />
+			</Col>
+			<Col>
+				<p>Todos los derechos estan reservados</p>
+			</Col>
+		</Row>
 	</footer>
 );
