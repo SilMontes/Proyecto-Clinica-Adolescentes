3a48e871aca092b22e2f3aba3ad14e11384da310
Kimur1 <kimur53@gmail.com>
2021-04-15 23:06:21 +0000

    navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,15 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { NavLink, N } from "react-bootstrap";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Clínica de Adolescentes</span>
 			</Link>
-			<div className="ml-auto">
+
+			<div className="float-right d-flex flex-row ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<p className="enter">Iniciar Sección</p>
+				</Link>
+				<p> - </p>
+				<Link to="/demo">
+					<p className=" enter ">Registrarse</p>
+				</Link>
+			</div>
+			<div className=" menu col-12 mr-auto">
+				<Link className="menu" href="/orientacion">
+					Orientacion
+				</Link>
+				<Link className="menu" href="/derechos">
+					Derechos
+				</Link>
+				<Link className="menu" href="#link">
+					Expertos
+				</Link>
+				<Link className="menu" href="#link">
+					Reportar
 				</Link>
 			</div>
 		</nav>
